7f91087d30fe2dee20f1c1fd86f328adc7f7af91
Alessandro Feitoza <06114387392@DIGITALCOLLEGE.LOCAL>
2023-09-20 09:49:11 -0300

    Aula 04

---
+++ aula04/conexao.js
@@ -0,0 +1,17 @@
+const mysql2 = require('mysql2/promise');
+
+async function executar(sql) {
+    let conexao = await mysql2.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '1234',
+        database: 'db_escola',
+        port: '3306',
+    });
+
+    let [results] = await conexao.query(sql);
+    
+    return results;
+}
+
+module.exports = {executar};
---
+++ aula04/index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const db = require('./conexao'); 
+
+async function recepcao(req, res) {
+    if (req.url === '/alunos') {
+        let dados = await db.executar('SELECT * FROM tb_aluno');
+        res.end(JSON.stringify(dados));
+        return;
+    }
+
+    if (req.url === '/labs') {
+        let dados = await db.executar('SELECT * FROM tb_lab');
+        res.end(JSON.stringify(dados));
+        return;
+    }
+
+    
+    if (req.url === '/professores') {
+        let dados = await db.executar('SELECT * FROM tb_professor');
+        res.end(JSON.stringify(dados));
+        return;
+    }
+
+    res.end('API ta ON');
+}
+
+http.createServer(recepcao).listen(8000);
+
+
